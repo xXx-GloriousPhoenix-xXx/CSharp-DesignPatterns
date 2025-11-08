d08a385558cef0af0683d80df222f4b795d036af
RomanPR <prikhodjko.roman@gmail.com>
2025-11-08 06:14:52 +0200

    prototype add

--- DesignPatterns/DesignPatterns/Program.cs
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DesignPatterns.Builder;
 using DesignPatterns.Factory;
+using DesignPatterns.Prototype;
 
 namespace DesignPatterns
 {
@@ -7,11 +8,29 @@ namespace DesignPatterns
     {
         public static void Main()
         {
-            var standardHouse = ConstructionEngineer.ConstructStandardHouse();
-            Console.WriteLine(standardHouse.DisplayInfo());
+            //Create custom circle
+            var myCircle = new Circle()
+            {
+                Color = "Red",
+                Radius = 10,
+                X = 5
+            };
+            Console.WriteLine(myCircle.GetInfo());
 
-            var luxuryHouse = ConstructionEngineer.ConstructLuxuryHouse();
-            Console.WriteLine(luxuryHouse.DisplayInfo());
+            //Copy circle
+            var myCircleCopy = myCircle.Clone();
+            Console.WriteLine(myCircleCopy.GetInfo());
+
+            //Initialize shape registry & Load default shapes
+            var registry = new ShapeRegistry();
+            registry.LoadDefaultShapes();
+
+            //Get default rectangle from registry
+            var defaultRectangle = registry.GetPrototype("Rectangle");
+            Console.WriteLine(defaultRectangle.GetInfo());
+
+            //Changing clone without changing original instance
+            // ?
         }
     }
 }
--- DesignPatterns/DesignPatterns/obj/Debug/net8.0/DesignPatterns.AssemblyInfo.cs
+++ DesignPatterns/DesignPatterns/obj/Debug/net8.0/DesignPatterns.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesignPatterns")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c97478967cfcc54598e1a1912a4a7e4aaa933407")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesignPatterns")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesignPatterns")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
