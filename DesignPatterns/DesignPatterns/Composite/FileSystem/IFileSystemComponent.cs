c53fc78a751ab0a8ba0d6f03848a9bb6b154dc44
RomanPR <prikhodjko.roman@gmail.com>
2025-11-09 20:03:16 +0200

    a

---
+++ DesignPatterns/DesignPatterns/Composite/FileSystem/IFileSystemComponent.cs
@@ -0,0 +1,74 @@
+﻿namespace DesignPatterns.Composite.MenuSystem
+{
+    public interface IFileSystemComponent
+    {
+        public string GetName();
+        public int GetSize();
+        public void Display(int indent = 0);
+        public IFileSystemComponent? ParentComponent { get; set; }
+    }
+    public class File(string name, int size, IFileSystemComponent? parentComponent = null) : IFileSystemComponent
+    {
+        private readonly string name = name;
+        private readonly int size = size;
+        public IFileSystemComponent? ParentComponent { get; set; } = parentComponent;
+        public string GetName() => name;
+        public int GetSize() => size;
+        public void Display(int indent = 0)
+        {
+            var stringSize = SizeConverter.GetSizeString(size);
+            var stringIndent = new string(' ', 3 * indent);
+            Console.WriteLine($"{stringIndent}{name} ({stringSize})");
+        }
+
+    }
+    public class Directory(string name, IFileSystemComponent? parentComponent = null) : IFileSystemComponent
+    {
+        private readonly string name = name;
+        public IFileSystemComponent? ParentComponent { get; set; } = parentComponent;
+        private readonly List<IFileSystemComponent> fileComponents = [];
+        public string GetName() => name;
+        public int GetSize()
+        {
+            return fileComponents.Select(fc => fc.GetSize()).Sum();
+        }
+        public void Add(IFileSystemComponent file)
+        {
+            file.ParentComponent = this;
+            fileComponents.Add(file);
+        }
+        public void Remove(IFileSystemComponent file)
+        {
+            fileComponents.Remove(file);
+        }
+        public void Display(int indent = 0)
+        {
+            var stringIndent = new string(' ', 3 * indent);
+            Console.WriteLine($"{stringIndent}{name}/");
+            foreach (var fc in fileComponents)
+            {
+                fc.Display(indent + 1);
+            }
+            if (ParentComponent is null)
+            {
+                var size = GetSize();
+                var sizeString = SizeConverter.GetSizeString(size);
+                Console.WriteLine($"Total size of {name}: {sizeString}");
+            }
+        }
+    }
+    public static class SizeConverter
+    {
+        private static readonly List<string> sizeNames = ["B", "KB", "MB", "GB", "TB", "PB"];
+        public static string GetSizeString(int byteSize)
+        {
+            var i = 0;
+            while (byteSize > 1024)
+            {
+                byteSize /= 1024;
+                i++;
+            }
+            return $"{byteSize} {sizeNames[i]}";
+        }
+    }
+}
--- DesignPatterns/DesignPatterns/obj/Debug/net8.0/DesignPatterns.AssemblyInfo.cs
+++ DesignPatterns/DesignPatterns/obj/Debug/net8.0/DesignPatterns.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesignPatterns")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f47f33f4fceb86a2eecef74a92b8c135adbbd69e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60e50eb918296601335b54e36c73c37310021f92")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesignPatterns")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesignPatterns")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
